761beaa97bc3d7cbca30e42efcfdaa6a2c9e8c13
Anderson Jacques <gabi.saiago@gmail.com>
2019-09-29 17:30:06 -0300

    Att1
    Conexão do banco

--- ControleVotos.php
+++ ControleVotos.php
@@ -1,5 +1,6 @@
 <?php
 	include "Conexao.php";
+	include 'Conexao.php';
 	require_once 'Presidente.php';
 	$presidente1 = new Presidente();
 	$presidente2 = new Presidente();
@@ -9,6 +10,9 @@
 
 	$sql = 'SELECT * FROM presidente WHERE 1 ';
 	echo "$digP";
-
+	$sql = "SELECT * FROM presidente";
+	$resultado = mysqli_query($conexao,$sql);
+	var_dump($resultado);
 	//require_once 'index.php';
+	
 ?>
